7a3c5537ee41e8e7f059b0338c472ae80bbfab9c
Chris Paquin <robotunderpants@gmail.com>
2025-12-28 12:13:34 -0500

    cleanup

--- deep-clean.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-#Run the standard cleanup plus delete local volumes
-#
-# 2. Wipe the persistent data directories
-sudo rm -rf /var/lib/zabbix/postgres/*
-sudo rm -rf /var/lib/zabbix/snmptraps/*
